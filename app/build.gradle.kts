d7a2311116b4653f50b81e86490222ef57bf1a33
Daria Trofimova <daria.trofimova.inbox@gmail.com>
2025-04-29 19:32:12 +0600

    Update libraries versions. Remove useless constraintlayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
