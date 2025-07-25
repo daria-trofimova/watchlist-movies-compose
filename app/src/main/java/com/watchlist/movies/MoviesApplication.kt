2587bf9273121503b5943ef6d3294cbdfdecdc38
Daria Trofimova <daria.trofimova.inbox@gmail.com>
2025-07-26 00:40:46 +0600

    Enable StrictMode Thread Policy in debug builds
    This change enables StrictMode in debug builds to help detect accidental disk or network access on the main thread.
    StrictMode is configured to log violations, but not crash the app.

--- app/src/main/java/com/watchlist/movies/MoviesApplication.kt
+++ app/src/main/java/com/watchlist/movies/MoviesApplication.kt
@@ -1,7 +1,24 @@
 package com.watchlist.movies
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy.Builder
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-internal class MoviesApplication : Application()
+internal class MoviesApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setStrictModePolicy()
+    }
+
+    private fun setStrictModePolicy() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                Builder().detectAll().penaltyLog().build(),
+            )
+        }
+    }
+}
